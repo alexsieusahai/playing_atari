a174d30f9e71a95f4a7b6a4535774ce84a2de846
alexsieusahai <alexsieu14@gmail.com>
2019-03-13 05:26:18 -0600

    Finally trains, not sure if implementation is correct though

--- dqn/deep_q_learning.py
+++ dqn/deep_q_learning.py
@@ -2,6 +2,7 @@ import gym
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
+import torch.autograd as autograd
 import numpy as np
 
 from experience_replay_buffer import ExperienceReplayBuffer
@@ -9,28 +10,38 @@ from function_approximator import ConvolutionApproximator
 from preprocess import phi
 
 
+Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs)
+
 def update_weights(Q, optimizer, experience_dataset, gamma) -> None:
     """
     I'm not sure if we're supposed to do it step by step like below, or
     as a whole batch... If we do it step by step, then we have Q-learning
     """
+    device = torch.device('cuda')
+    total_loss = 0
     for state, action, reward, next_state in experience_dataset:
-        q_values = Q(state)
-        next_q_values = Q(next_state)
+        state = Variable(torch.DoubleTensor(np.float32(state)))
+        action = Variable(torch.LongTensor(np.float32([action])))
+        reward = Variable(torch.DoubleTensor(np.float32([reward])).to(device))
+        next_state = Variable(torch.DoubleTensor(np.float32(next_state)))
 
-        q_value_for_action = float(q_values[0][action])
-        max_q_for_next = float(next_q_values.max(1)[0])
+
+        q_values = Q(state.to(device))
+        next_q_values = Q(next_state.to(device))
+
+        q_value_for_action = q_values.gather(1, action.unsqueeze(1).to(device)).squeeze(1)
+        max_q_for_next = next_q_values.max(1)[0]
         
         expected_q_value = reward + gamma * max_q_for_next
 
-        loss = F.mse_loss(q_value_for_action, expected_q_value)
+        loss = (q_value_for_action - Variable(expected_q_value.data)).pow(2)
+        total_loss += loss
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-    return loss
-
+    return total_loss / len(experience_dataset)
 
 
 def deep_q_learning(memory_max_capacity: int, num_episodes: int, minibatch_size: int,
@@ -46,12 +57,13 @@ def deep_q_learning(memory_max_capacity: int, num_episodes: int, minibatch_size:
     :returns: A function approximator.
     """
     env = gym.make('Breakout-v0')
-    device = torch.device('cpu')
+    device = torch.device('cuda')
     replay_memory = ExperienceReplayBuffer(memory_max_capacity)
     Q = ConvolutionApproximator(4).double().to(device)  # 4 is amount of moves in breakout
     optimizer = optim.RMSprop(Q.parameters())
 
     for num_episode in range(num_episodes):
+        print(f'Episode {num_episode}')
         phi_dict = {}
         obs = env.reset()
         obs_list = [obs] * 4
@@ -63,9 +75,9 @@ def deep_q_learning(memory_max_capacity: int, num_episodes: int, minibatch_size:
             if np.random.random() < epsilon:
                 action = env.action_space.sample()
             else:
-                output = Q(state)
+                output = Q(state.to(device))
                 action = output.max(1)[1].view(1, 1)
-            
+
             previous_state = state
             obs, reward, done, _ = env.step(action)
             obs_list.append(obs)
@@ -77,6 +89,12 @@ def deep_q_learning(memory_max_capacity: int, num_episodes: int, minibatch_size:
 
             update_weights(Q, optimizer, experience_sample, gamma)
 
+    return Q, optimizer
+
 
 if __name__ == "__main__":
-    deep_q_learning(10e7, 1, 32, 0.99)
+    import pickle as pkl
+
+    Q, optimizer = deep_q_learning(10e7, 1, 32, 0.99)
+    pkl.dump(Q, open('Q.pkl', 'wb'))
+    pkl.dump(optimizer, open('optimizer.pkl', 'wb'))
